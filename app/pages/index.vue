1c04084cd95463eccb555430514bdb09f9e5a9f8
Pei Wu <starwu02@gmail.com>
2025-12-18 18:10:35 +0800

    refactor: 完整重構，kinect互動待處理

---
+++ app/pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="relative inset-0 h-full">
+		<img src="~/assets/images/cover.png" class="h-full" />
+
+		<div
+			class="button-bounce flex justify-center items-center top-[66%] left-[28%] absolute group rotate-4 z-10"
+		>
+			<button
+				class="kinect-interactive flex justify-center items-center group bg-[#186DC4] w-60 h-20 rounded-full outline-black outline-4 z-20"
+				@click="handleStateChange"
+			>
+				<img src="~/assets/images/start.png" class="-rotate-4" />
+			</button>
+
+			<!-- <img
+				src="~/assets/images/button_hover.png"
+				class="absolute max-w-none group-hover:opacity-100 opacity-0"
+			/> -->
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const handleStateChange = () => {
+	router.push("/question1");
+};
+</script>
---
+++ app/pages/question2.vue
@@ -0,0 +1,51 @@
+<!-- 問題二 -->
+<template>
+	<div class="flex relative justify-center items-center h-full">
+		<img src="~/assets/images/bg/2.png" class="h-full" />
+
+		<div
+			class="absolute top-0 size-full flex flex-col items-center justify-center gap-10"
+		>
+			<div
+				class="flex flex-col justify-center items-center font-bold text-black text-4xl gap-2"
+			>
+				<h1 class="">以下哪種情境</h1>
+				<h1 class="">最讓您感到壓力？</h1>
+			</div>
+
+			<div class="flex flex-col items-center justify-center w-80 gap-4">
+				<div v-for="option in questionOptions.Q2" :key="option" class="w-full">
+					<button
+						class="kinect-interactive bg-[#E6FFE9] border-4 border-[#29983C] rounded-full w-full h-20 hover:scale-110 transition-transform duration-300 p-4"
+						@click="handleOptionSelect(option)"
+					>
+						{{ option }}
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const questionOptions = {
+	Q1: ["cat", "dog", "lion", "owl", "penguin", "whale"],
+	Q2: ["決定午餐要吃什麼", "跟店員多要一份餐具", "拒絕朋友不想去的聚會"],
+	Q3: [
+		"雖受傷但認同，急著想知道如何改進！",
+		"表面點頭道謝，心裡覺得對方根本不懂⋯⋯⋯",
+		"堅信自己最棒，無視批評並吃甜點犒賞自己",
+	],
+	Q4: ["吃飯的時候", "看電影的時候", "讀書的時候"],
+};
+
+const handleOptionSelect = (option: string) => {
+	console.log("Selected option:", option);
+
+    router.push("/question3");
+};
+</script>
